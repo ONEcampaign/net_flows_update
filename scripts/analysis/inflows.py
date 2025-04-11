d6c3081ac6743616b7b61f80380300b9eb24ca4e
Jorge Rivera <jorge.rivera@one.org>
2025-04-11 11:30:49 +0200

    7 data for inflows chart (#11)
    * Add historical inflows
    
    * avg charts
    
    * Update inflows.py
    
    * Update debt_service.py

--- scripts/analysis/inflows.py
+++ scripts/analysis/inflows.py
@@ -4,10 +4,12 @@ from scripts.analysis.common import (
     all_flows_pipeline,
     AnalysisVersion,
     create_dev_countries_total,
+    AVERAGE_PERIODS,
 )
 
 
 def historical_inflows(
+    debt_only: bool = False,
     version: AnalysisVersion = "total",
     china_as_counterpart_type: bool = False,
     exclude_outliers: bool = True,
@@ -23,6 +25,11 @@ def historical_inflows(
         china_as_counterpart_type=china_as_counterpart_type,
         constant=constant,
     ).loc[lambda d: d.indicator_type == "inflow"]
+
+    if debt_only:
+        inflows_data = inflows_data.loc[
+            lambda d: ~d.indicator.isin(["grants_bilateral", "grants_multilateral"])
+        ]
 
     total_data = create_dev_countries_total(data=inflows_data)
 
@@ -44,5 +51,52 @@ def historical_inflows(
     return inflows_data
 
 
+def inflows_by_period(
+    inflows_data: pd.DataFrame, china_as_counterpart_type: bool = True
+) -> pd.DataFrame:
+    """
+    Group inflows data by specified AVERAGE_PERIODS and calculate average values.
+
+    Args:
+        inflows_data (pd.DataFrame): The data to group.
+        china_as_counterpart_type (bool): Whether to treat China as a counterpart type.
+    Returns:
+        pd.DataFrame: The grouped DataFrame with average values.
+
+    """
+
+    inflows_data = inflows_data.copy(deep=True)
+
+    for period, settings in AVERAGE_PERIODS.items():
+        start_year, end_year = settings["years"]
+        inflows_data.loc[
+            (inflows_data.year >= start_year) & (inflows_data.year <= end_year),
+            "period",
+        ] = period
+
+    # Remove rows with NaN in the 'period' column
+    data = inflows_data.dropna(subset=["period"])
+
+    # Grouper
+    grouper = ["period", "country", "continent", "income_level", "prices"]
+
+    if china_as_counterpart_type:
+        grouper.append("counterpart_type")
+
+    # Group by period and sum the values
+    data = (
+        data.groupby(grouper, observed=True, dropna=False)["value"].sum().reset_index()
+    )
+
+    data["length"] = data["period"].map(
+        lambda x: AVERAGE_PERIODS[x]["length"] if x in AVERAGE_PERIODS else None
+    )
+
+    data["value"] = (data["value"] / data["length"]).round(2)
+
+    return data.drop(columns=["length"])
+
+
 if __name__ == "__main__":
-    total_inflows = historical_inflows(china_as_counterpart_type=True)
+    total_inflows = historical_inflows(debt_only=True, china_as_counterpart_type=True)
+    total_inflows_avg = inflows_by_period(total_inflows, china_as_counterpart_type=True)
